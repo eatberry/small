5ca67aebcbae9672f8dcb1ec55a6b786a303e4a9
eatberry <1009452821@qq.com>
2021-01-30 00:33:59 +0800

    新建组织架构

---
+++ smallBase/smallPortal/src/main/java/com/smallBase/SmallPortalApplication.java
@@ -0,0 +1,11 @@
+package com.smallBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmallPortalApplication.class,args);
+    }
+}
